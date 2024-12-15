d405c95f0396ea92d99289b2d49bb8b85016f4e1
Sneha Mukteshwar Mogali <snehamogali@Snehas-MacBook-Air.local>
2024-12-15 20:21:18 +0530

    Initial commit for my microservice

---
+++ src/main/java/com/adsm/gtt_uservice/GttUserviceApplication.java
@@ -0,0 +1,13 @@
+package com.adsm.gtt_uservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GttUserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GttUserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adsm/gtt_uservice/GttUserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adsm.gtt_uservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GttUserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
